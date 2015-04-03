8e552edbf0841d22fa746cb6a21b722789eb86b6
jpheisel <jpheisel@gmail.com>
2015-04-03 11:25:03 -0500

    Token used to limit throttling of requests

--- get_data.py
+++ get_data.py
@@ -6,10 +6,14 @@
 
 from urllib import urlopen
 import json
+import csv
 
 
-url = urlopen('https://data.cityofchicago.org/resource/ijzp-q8t2.json').read()
+# Should use app token to prevent request throttling
+url = urlopen('https://data.cityofchicago.org/resource/ijzp-q8t2.json?$$app_token=riGeeqnG1oMsfm6AvIDNOoFzY').read()
+
 data = json.loads(url)
-print data[0:10]
+print data
+
 
 
