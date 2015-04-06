6f7e3d0ab0de59f2a111d24a6fed5cbe96b6dd0b
jpheisel <jpheisel@gmail.com>
2015-04-06 11:26:09 -0500

    Few changes

--- exploratory_scripting.R
+++ exploratory_scripting.R
@@ -14,9 +14,15 @@ data_ <- fread("chicago_crime.csv")
 burgs <- subset(data_, data_$"Primary Type" == "BURGLARY")
 sample_of_burgs <- burgs[ID %in% sample(burgs$ID, 175000, FALSE)]
 
+# Get only date
+burgs$date <- as.Date(burgs$Date, "%m/%d/%Y")
 
 # Summary statistics
 count(data_$"Primary Type")
+count(burgs$Ward)
+count(burgs$Beat)
+count(burgs$District)
+
 
 
 # Write out
